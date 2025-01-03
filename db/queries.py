70253f5cb70406edad27a1b9775f6eb028149899
DAVID789-ops <masterlol13@hotmail.com>
2025-01-03 00:45:36 -0600

    Descripción de los cambios realizados

--- db/queries.py
+++ db/queries.py
@@ -3,7 +3,7 @@ from db.connection import get_connection
 def obtener_deudas():
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT nombre_colegio, cantidad_deuda, producto_entregado, fecha_entrega, email_contacto, signo, intensidad, idioma, tiempo FROM deudas")
+    cursor.execute("SELECT nombre_colegio, cantidad_deuda, producto_entregado, fecha_entrega, email_contacto, signo, intensidad, idioma, tiempo, pdf_documento FROM deudas")
     registros = cursor.fetchall()
     conn.close()
     return registros
